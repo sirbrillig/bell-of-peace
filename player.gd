4359b9f57f71606f2b58d480fd163372390fe115
Payton Swick <payton@foolord.com>
2024-08-31 22:42:30 -0400

    Add tilesets

--- player.gd
+++ player.gd
@@ -25,7 +25,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
