cb66aad6ebfabcbea9d18178f0125811adeb63c2
Payton Swick <payton@foolord.com>
2024-08-31 21:29:14 -0400

    Use jump variable calc

--- player.gd
+++ player.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
-@export var speed = 100
-@export var jump_speed = -400
-@export var gravity = 30
+@export var speed: float
 
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height ) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height ) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+func calc_gravity() -> float:
+	if velocity.y < 0.0:
+		return jump_gravity
+	else:
+		return fall_gravity
+
+func jump() -> void:
+	velocity.y = jump_velocity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,15 +33,12 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.animation = "idle"
 
 func _physics_process(delta: float) -> void:
-	if ! is_on_floor():
-		velocity.y += gravity
-	else:
-		velocity.y = 0
+	velocity.y += calc_gravity() * delta
 	
 	var direction_h = Input.get_axis("ui_left", "ui_right")
 	velocity.x = speed * direction_h
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = jump_speed
-		
+		jump()
+	
 	move_and_slide()
