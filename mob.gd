4c115d577af68835024a5c56931a87518dc24e99
Payton Swick <payton@foolord.com>
2024-09-01 12:51:29 -0400

    Let Mobs jump to follow player

--- mob.gd
+++ mob.gd
@@ -3,9 +3,25 @@ extends CharacterBody2D
 @export var speed: float
 @export var attack_range: float
 @export var aware_distance: float
-@export var gravity: float
 var player_position: Vector2
 @onready var player: CharacterBody2D = get_node("../Player")
+
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height ) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height ) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+func calc_gravity() -> float:
+	if velocity.y < 0.0:
+		return jump_gravity
+	else:
+		return fall_gravity
+
+func jump() -> void:
+	velocity.y = jump_velocity
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,9 +41,15 @@ func _physics_process(delta: float) -> void:
 	player_position = player.position
 	var distance_to_player = position.distance_to(player_position)
 	if distance_to_player > attack_range and distance_to_player < aware_distance:
-		velocity.x = (position.direction_to(player_position) * speed).x
+		move_toward_player()
 	else:
 		velocity.x = 0
-	if !is_on_floor():
-		velocity.y += gravity
+	velocity.y += calc_gravity() * delta
+	
+	if is_on_wall():
+		jump()
+	
 	move_and_slide()
+	
+func move_toward_player() -> void:
+	velocity.x = (position.direction_to(player_position) * speed).x
