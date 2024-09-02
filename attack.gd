68015fdad11c89b5e68e246470d41966620adcdf
Payton Swick <payton@foolord.com>
2024-09-01 20:44:08 -0400

    Fix attack to only hit player when frame is correct

--- attack.gd
+++ attack.gd
@@ -2,16 +2,19 @@ extends State
 class_name Attack
 
 @export var enemy: Mob
+@export var attack_shape: CollisionShape2D
 @export var animation: AnimatedSprite2D
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
 
 
 func update(_delta):
-	if animation.frame > 2:
-		enemy.is_attacking = true
+	if enemy.is_attacking and animation.frame > 2:
+		attack_shape.set_deferred("disabled", false)
+	enemy.is_attacking = true
 	
 func exit():
 	enemy.is_attacking = false
+	attack_shape.set_deferred("disabled", false)
 	
 
 func _on_attack_area_area_entered(area) -> void:
--- mob.gd
+++ mob.gd
@@ -13,9 +13,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if is_attacking:
 		$AnimatedSprite2D.animation = "attack"
-		$AttackArea/AttackShape.set_deferred("disabled", false)
 		return
-	$AttackArea/AttackShape.set_deferred("disabled", true)
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
 		return
